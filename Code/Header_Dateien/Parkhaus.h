fa6357cf8e9955ceb552fbcd644a99423fb7e483
Marc Born <marc.born.mb@gmail.com>
2026-02-24 19:07:01 +0100

    Variabeln für Fahrzeug definiert

--- Code/Header_Dateien/Parkhaus.h
+++ Code/Header_Dateien/Parkhaus.h
@@ -2,7 +2,10 @@
 #define PARKHAUS_H
 
 typedef struct {
-    
+    int fahrzeug_id;                 /* Eindeutige ID des Fahrzeugs */
+    int verbleibende_parkdauer;      /* Restdauer der Parkzeit */
+    int eintritts_zeit;              /* Zeitschritt beim Einfahren */
+    int wartezeit;                   /* Wartezeit vor Einfahrt */
 } Fahrzeug;
 
 typedef struct
