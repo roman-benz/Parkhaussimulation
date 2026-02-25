4e4438d7894101a1e685485a08dbd69264b67adf
Marc Born <marc.born.mb@gmail.com>
2026-02-25 08:35:38 +0100

    Ich hab jetzt den kompletten Konfig.h Header erstellt (fixes #4)

--- Code/Header_Dateien/Konfig.h
+++ Code/Header_Dateien/Konfig.h
@@ -1,6 +1,10 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+
+/**
+ * @brief Umfasst alle zur Laufzeit konfigurierbaren Simulationsparameter.
+ */
 typedef struct
 {
     int anzahl_parkplaetze;
@@ -8,8 +12,16 @@ typedef struct
     int anzahl_simulationsschritte;
     int ankunftswahrscheinlichkeit_prozent;
     unsigned int zufalls_seed; 
-} Simulatiionskonfiguration;
+} Simulationskonfiguration;
 
-bool lese_und_validiere_konfiguration(imulationskonfiguration *p_konfiguration);
+
+/**
+ * @brief Liest alle Eingaben aus dem Terminal und validiert diese.
+ *
+ * @param[out] p_konfiguration Zielstruktur für die eingelesenen Parameter.
+ * @return true, wenn eine gültige Konfiguration eingelesen wurde.
+ */
+int konfiguration_einlesen(Simulationskonfiguration *p_konfiguration);
+
 
 #endif
