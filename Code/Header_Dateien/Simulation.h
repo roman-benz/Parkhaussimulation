55b1f13c0a84e386051e6369fcf83939a9fe9b2b
Julien Armbruster <Julien.armbruster06@gmail.com>
2026-02-25 10:31:05 +0100

    SimulationDaten Struct erstellt und Funktionsprototyp für ausführen_simulationsschritt erstellt

--- Code/Header_Dateien/Simulation.h
+++ Code/Header_Dateien/Simulation.h
@@ -2,10 +2,30 @@
 #define SIMULATION_H
 
 #include "Parkhaus.h"
+#include "Konfig.h"
+#include "Queue.h"
+
+typedef struct {
+    int gesamt_ankuenfte;
+    int gesamt_geparkt;
+    int gesamt_abfahrten;
+    int gesamt_abgewiesen;
+    int warteschlangen_laenge;
+    double auslastungsrate;
+    double durchschnittliche_wartezeit;
+} SimulationDaten;
 
 /**
  * @brief Initialisiert Parkhausdatenstrukturen für die Simulation.
  */
 void initialisierung_garage(Parkhaus *garage, int maximale_kapazitaet);
+
+void ausführen_simulationsschritt(
+    int current_step,
+    const Simulationskonfiguration *p_konfiguration,
+    Parkhaus *garage,
+    Queue *queue,
+    SimulationDaten *daten
+);
 
 #endif
