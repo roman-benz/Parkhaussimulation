125768993206a169438cda52b92e4b4e9b59613c
Julien Armbruster <Julien.armbruster06@gmail.com>
2026-02-25 08:54:59 +0100

    Funktionsprototyp zur Inittialisierung der Garage erstellt

---
+++ Code/Header_Dateien/Simulation.h
@@ -0,0 +1,11 @@
+#ifndef SIMULATION_H
+#define SIMULATION_H
+
+#include "Parkhaus.h"
+
+/**
+ * @brief Initialisiert Parkhausdatenstrukturen für die Simulation.
+ */
+void initialisierung_garage(Parkhaus *garage, int maximale_kapazitaet);
+
+#endif
