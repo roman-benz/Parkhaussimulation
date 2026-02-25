629130ef822f64974520bc772f013475f1a45b30
Marc Born <marc.born.mb@gmail.com>
2026-02-25 08:25:00 +0100

    Variabeln erstellt

--- Code/Header_Dateien/Konfig.h
+++ Code/Header_Dateien/Konfig.h
@@ -3,7 +3,11 @@
 
 typedef struct
 {
-    
+    int anzahl_parkplaetze;
+    int max_parkdauer_minuten;
+    int anzahl_simulationsschritte;
+    int ankunftswahrscheinlichkeit_prozent;
+    unsigned int zufalls_seed; 
 } Simulatiionskonfiguration;
 
 #endif
