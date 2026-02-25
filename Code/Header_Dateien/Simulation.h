cea8cbaebe53fe11397b228ccabec64b7432cc58
Julien Armbruster <Julien.armbruster06@gmail.com>
2026-02-25 10:47:01 +0100

    Einheitlicher Header für Funktionsprototypen (Fixes #11)

--- Code/Header_Dateien/Simulation.h
+++ Code/Header_Dateien/Simulation.h
@@ -17,19 +17,42 @@ typedef struct {
 
 /**
  * @brief Initialisiert Parkhausdatenstrukturen für die Simulation.
+ * 
+ * @param p_garage Zeiger auf die zu initialisierende Parkhausstruktur
+ * @param maximale_kapazitaet Maximale Anzahl der Stellplätze
  */
-void initialisierung_garage(Parkhaus *garage, int maximale_kapazitaet);
+void initialisierung_garage(Parkhaus *p_garage, int maximale_kapazitaet);
 
+/**
+ * @brief Führt einen einzelnen Simulationsschritt aus.
+ * 
+ * @param aktueller_schritt Nummer des aktuellen Simulationsschritts
+ * @param p_konfiguration Zeiger auf die Simulationskonfiguration
+ * @param p_garage Zeiger auf das Parkhaus
+ * @param p_queue Zeiger auf die Warteschlange
+ * @param p_daten Zeiger auf die Datenstruktur zur Speicherung der Ergebnisse
+ */
 void ausführen_simulationsschritt(
     int aktueller_schritt,
     const Simulationskonfiguration *p_konfiguration,
-    Parkhaus *garage,
-    Queue *queue,
+    Parkhaus *p_garage,
+    Queue *p_queue,
     Simulationdaten *p_daten
 );
 
+/**
+ * @brief Gibt die Simulationsdaten eines Schritts auf der Konsole aus.
+ * 
+ * @param aktueller_schritt Nummer des aktuellen Simulationsschritts
+ * @param p_daten Zeiger auf die auszugebenden Simulationsdaten
+ */
 void simulationsschrittdaten_ausgeben(int aktueller_schritt, const Simulationdaten *p_daten);
 
+/**
+ * @brief Führt die komplette Parkhaus-Simulation aus.
+ * 
+ * @param p_konfiguration Zeiger auf die Simulationskonfiguration
+ */
 void run_simulation(const Simulationskonfiguration *p_konfiguration);
 
 #endif
