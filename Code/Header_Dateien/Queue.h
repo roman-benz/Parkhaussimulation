edcd1c9eda2df21d140cc3797ffddb9206408e17
Roman Benz <kontakt@romanbenz.com>
2026-02-25 08:45:36 +0100

    Added struct Queue

--- Code/Header_Dateien/Queue.h
+++ Code/Header_Dateien/Queue.h
@@ -3,10 +3,18 @@
 
 #include "Parkhaus.h"
 
-typedef struct QueueNode {
+typedef struct QueueNode 
+{
     Fahrzeug einFahrzeug;
     struct QueueNode *next;
 } QueueNode;
+
+typedef struct Queue 
+{
+    QueueNode *head;
+    QueueNode *tail;
+    int length;
+} Queue;
 
 
 #endif
