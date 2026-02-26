2741aa73cf6e4002f9b239e3fe4defb213cd7900
Roman Benz <kontakt@romanbenz.com>
2026-02-26 11:54:54 +0100

    Added function Dequeue to remove cars from queue when they park. Commentet it out aswell

--- Code/src/Queue.c
+++ Code/src/Queue.c
@@ -24,9 +24,29 @@ Function queue_enqueue(Queue *eineQueue, Fahrzeug *einFahrzeug, int enqueue_zeit
             Fahrzeugknoten->next <- NULL;
             Fahrzeugknoten->enqueue_zeitschritt <- enqueue_zeitschritt;
         END IF 
-        eineQueue->length <- eineQueue->(length+1); 
+        eineQueue->length <- eineQueue->length + 1; 
 
     ELSE 
         END 
     END IF 
-    END
+END
+
+Function queue_dequeue(Queue *eineQueue, int *einparken_zeitschritt)
+    //Wenn die Warteschlange leer ist -> Abbruch
+    IF eineQueue->length == 0
+        return NULL;        //Abbruch
+    ELSE 
+    //Wenn Warteschlange nicht leer ist
+        QueueNode *entfernterKnoten <- eineQueue->head;     //neuen Knoten definieren, der Head entspricht
+        einparkendesFahrzeug <- entfernterKnoten->einFahrzeug;      //Das Fahrzeug festlegen, um später das Fahrzeug returnen zu können
+        einparkendesFahrzeug->wartezeit <- *einparken_zeitschritt - entfernterKnoten->enqueue_zeitschritt;       //Wartezeit ausrechnen (Zeitschritt beim Einfahren ins Parkhaus - Zeitschritt beim Eintritt in Warteschlange)
+        
+        eineQueue->head <- entfernterKnoten->next;      //Den Head auf das nächste Fahrzeug in der Warteschlange legen
+        IF (eineQueue->head == NULL) 
+            eineQueue->tail = NULL;
+        END IF
+        free(entfernterKnoten);     //Den Speicher des Knoten des einparkenden Autos freigeben
+        eineQueue->length <- eineQueue->length - 1;     //Die Länge der Warteschlange um 1 reduzieren
+        return einparkendesFahrzeug;        //Das einzuparkende Auto returnen
+    END IF 
+END
