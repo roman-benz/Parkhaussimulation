ae0d51fd84d00115f8dcd550e08cf7dbb64f64e4
Roman Benz <kontakt@romanbenz.com>
2026-02-27 12:15:07 +0100

    Speicherreservierung korrigiert (fixes #37)

--- Code/src/Queue.c
+++ Code/src/Queue.c
@@ -9,7 +9,7 @@ END
 
 Function queue_enqueue(Queue *eineQueue, Fahrzeug *einFahrzeug, int enqueue_zeitschritt)
     //Neuer Listenelement für das Fahrzeug mit Malloc reserviert
-    QueueNode *Fahrzeugknoten <- malloc(sizeof(Fahrzeugknoten));
+    QueueNode *Fahrzeugknoten <- malloc(sizeof *Fahrzeugknoten);
     IF (malloc() erfolgreich?)
         //Das Fahrzeug das der Schlange hinzugefügt werden soll wird hier dem Knoten zugewiesen
         Fahrzeugknoten->einFahrzeug <- einFahrzeug;
