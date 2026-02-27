b186d9706aa38183d45b6d62fa8abd4f8d216bbe
Marc Born <marc.born.mb@gmail.com>
2026-02-27 14:24:39 +0100

    Main funktion erstellt

--- Code/src/Main.c
+++ Code/src/Main.c
@@ -1,18 +1,8 @@
-// Pseudocode für Main.c
+#include "../Include/Konfig.h"
+#include "../Include/Simulation.h"
+#include <stdio.h>
+
 
 Function main()
-    // Initialisiere Konfigurationsstruktur
-    SimulationConfig config
-
-    PRINT "=== Parkhaus-Simulation (Teil I: Programmgerüst) ==="
-
-    IF NOT read_and_validate_config(config)
-        PRINT_ERROR "Konfiguration konnte nicht eingelesen werden."
-        RETURN 1
-    END IF
-
-    // Starte die Simulation mit der geladenen Konfiguration
-    run_simulation(config)
-
-    RETURN 0
+    
 END
