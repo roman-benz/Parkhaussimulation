1ca3aef016b738c0dd577cb884cd57026ef44b8c
Julien Armbruster <Julien.armbruster06@gmail.com>
2026-02-27 10:04:18 +0100

    einparken_Fahrzeug in Pseudocode geschrieben

--- Code/src/Simulation.c
+++ Code/src/Simulation.c
@@ -18,12 +18,33 @@ Function initialisierung_garage(Parkhaus *p_garage, int maximale_kapazitaet)
 		RETURN;     //Speicher konnte nicht reserviert werden
 	END IF
 
-	//Alle Stellplaetze mit Leerwerten initialisieren
-	FOR i = 0 TO maximale_kapazitaet - 1
-		p_garage->p_stellplaetze[i].fahrzeug_id = -1;
+	//Alle Stellplaetze mit for Schleife mit Leerwerten initialisieren
+	FOR i = 0 TO maximale_kapazitaet - 1 DO
+		p_garage->p_stellplaetze[i].fahrzeug_id = -1; //-1 steht für einen leeren Parkplatz
 		p_garage->p_stellplaetze[i].verbleibende_parkdauer = 0;
 		p_garage->p_stellplaetze[i].eintritts_zeit = 0;
 		p_garage->p_stellplaetze[i].wartezeit = 0;
 	END FOR
+END
+
+Function einparken_fahrzeug(Parkhaus *p_garage, const Fahrzeug *p_fahrzeug)
+	IF (p_garage == NULL OR p_fahrzeug == NULL)
+		RETURN 0;   //Ungueltige Eingaben
+	END IF
+
+	IF (p_garage->belegte_stellplaetze >= p_garage->maximale_kapazitaet)
+		RETURN 0;   //Parkhaus ist voll
+	END IF
+
+	//Ersten freien Stellplatz suchen (frei = fahrzeug_id == -1)
+	FOR i = 0 TO p_garage->maximale_kapazitaet - 1 DO
+		IF (p_garage->p_stellplaetze[i].fahrzeug_id == -1) //Prüfung ob Parkplatz frei ist
+			p_garage->p_stellplaetze[i] = *p_fahrzeug; //Fahrzeugdaten auf Parkplatzplatz kopieren
+			p_garage->belegte_stellplaetze = p_garage->belegte_stellplaetze + 1;
+			RETURN 1;   //Einparken erfolgreich
+		END IF
+	END FOR
+
+	RETURN 0;   //Sicherheitsreturn (kein freier Platz gefunden)
 END
 
