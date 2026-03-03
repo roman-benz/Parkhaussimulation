7a582ac56d0b612476729e5bb884bdc6dbc85a00
Roman Benz <kontakt@romanbenz.com>
2026-03-03 18:23:08 +0100

    Changed naming of variables to match coding conventions

--- Code/Include/Queue.h
+++ Code/Include/Queue.h
@@ -14,7 +14,7 @@
 
 typedef struct QueueNode 
 {
-    Fahrzeug *einFahrzeug;
+    Fahrzeug *p_einFahrzeug;
     int enqueue_zeitschritt;
     struct QueueNode *next;
 } QueueNode;
@@ -45,24 +45,24 @@ void queue_init(Queue *queue);
  * 
  * Die Funktion erstellt einen neuen Knoten mit den Informationen des Fahrzeugs und fügt diesen am Ende der Queue hinzu.
  * 
- * @param[in, out] eineQueue Zeiger auf die Queue, in die das Fahrzeug eingefügt werden soll
- * @param[in] einFahrzeug Zeiger auf das Fahrzeug, das in die Queue eingefügt werden soll
+ * @param[in, out] p_eineQueue Zeiger auf die Queue, in die das Fahrzeug eingefügt werden soll
+ * @param[in] p_einFahrzeug Zeiger auf das Fahrzeug, das in die Queue eingefügt werden soll
  * @param[in] enqueue_zeitschritt der Zeitschritt, zu dem das Fahrzeug in die Queue eingefügt wurde (fuer spaetere Berechnung der Wartezeit!)
  * @return void
  */
-void queue_enqueue(Queue *eineQueue, Fahrzeug *einFahrzeug, int enqueue_zeitschritt);
+void queue_enqueue(Queue *p_eineQueue, Fahrzeug *p_einFahrzeug, int enqueue_zeitschritt);
 
 /**
  * @brief Entfernt ein Fahrzeug aus der Warteschlange
  * 
  * Die Funktion entfernt den Knoten am Anfang der Queue und gibt das darin enthaltene Fahrzeug zurück.
  * 
- * @param[in, out] eineQueue Zeiger auf die Queue, aus der das Fahrzeug entfernt werden soll
+ * @param[in, out] p_eineQueue Zeiger auf die Queue, aus der das Fahrzeug entfernt werden soll
  * @param[out] einparkendesFahrzeug Zeiger auf das Fahrzeug, das aus der Queue entfernt werden soll
  * @param[in] einparken_zeitschritt Zeitschritt, zu dem das Fahrzeug aus der Queue entfernt wurde (fuer spaetere Berechnung der Wartezeit!)
  * @return Das entfernte Fahrzeug
  */
-Fahrzeug* queue_dequeue(Queue *eineQueue, int einparken_zeitschritt);
+Fahrzeug* queue_dequeue(Queue *p_eineQueue, int einparken_zeitschritt);
 
 /**
  * @brief Gibt den Speicher der Queue frei
@@ -72,7 +72,7 @@ Fahrzeug* queue_dequeue(Queue *eineQueue, int einparken_zeitschritt);
  * @param[in, out] queue Zeiger auf die Queue, deren Speicher freigegeben werden soll
  * @return void
  */
-void queue_destroy(Queue *queue);   
+void queue_destroy(Queue *p_queue);   
 
 
 #endif
