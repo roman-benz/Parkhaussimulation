e7ac6e7e0f7663981e0b45008759b79c2956d25f
Roman Benz <kontakt@romanbenz.com>
2026-03-03 18:52:23 +0100

    Added argumentative description to queue_destroy

--- Code/src/Queue.c
+++ Code/src/Queue.c
@@ -70,6 +70,10 @@ Function queue_dequeue(Queue *eineQueue, int einparken_zeitschritt)
     
 END
 
+/*
+    Am Simulationsende muss jeglicher Speicher freigegeben werden. Das passiert hier, in dem wir jeden Knoten mithilfe von queue_dequeue freigeben.
+    queue_dequeue gibt den Speicher des jeweiligen Knotens frei.
+*/
 Function queue_destroy(Queue *queue)
 int platzhalter_Zeitschritt = 0;        //Platzhalter, da queue_dequeue einen Zeitschritt braucht
     while queue_dequeue(queue, Platzhalter_Zeitschritt) IS NOT NULL       //Solange Queue_dequeue nicht NULL zurückliefert, wird die Funktion erneut aufgerufen
