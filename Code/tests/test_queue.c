8203ec8e43cb85545f12fad6b37ff835fb49be8e
Roman Benz <kontakt@romanbenz.com>
2026-03-14 21:33:24 +0100

    Wrote first test for queue_init(): Checked if the function sets head, tail and length to NULL NULL and or when given an random queue

--- Code/tests/test_queue.c
+++ Code/tests/test_queue.c
@@ -6,3 +6,15 @@
 #include "../Include/Simulation.h"
 
 
+void test_queue_init_setzt_felder_auf_null(void)
+{
+    Queue q;
+    queue_init(&q);
+
+    assert(q.head == NULL);
+    assert(q.tail == NULL);
+    assert(q.length == 0);
+
+    printf("test_queue_init_setzt_felder_auf_null: OK\n");
+}
+
