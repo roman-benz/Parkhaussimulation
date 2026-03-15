dff7d761015755eb06ecd094def453f27068e9b0
Roman Benz <kontakt@romanbenz.com>
2026-03-15 16:50:07 +0100

    Wrote first test that checks if a car is added to the queue correctly if nno parking spaces are available

--- Code/tests/test_simulation.c
+++ Code/tests/test_simulation.c
@@ -5,3 +5,37 @@
 #include <stdlib.h>
 #include "../Include/Simulation.h"
 
+
+void test_simulationsschritt_ankunft_geht_in_queue_wenn_voll(void)
+{
+    Parkhaus garage;
+    initialisierung_garage(&garage, 1);
+
+    Fahrzeug geparktes_auto = {1, 99, 0, 0}; 
+    einparken_fahrzeug(&garage, &geparktes_auto);
+
+    Queue queue;
+    queue_init(&queue);
+
+    Simulationdaten daten = {0, 0, 0, 0, 0, 0, 0.0, 0.0, 0.0};
+
+    Simulationskonfiguration konfig = {1, 10, 100, 100, 42};
+    srand(konfig.zufalls_seed);
+
+    ausfuehren_simulationsschritt(1, &konfig, &garage, &queue, &daten);
+
+    assert(daten.gesamt_ankuenfte == 1);   
+    assert(queue.length == 1);             
+    assert(daten.gesamt_geparkt == 0);      
+    assert(garage.belegte_stellplaetze == 1); 
+
+    free(garage.p_stellplaetze);
+    queue_destroy(&queue);
+    printf("simulationsschritt_ankunft_geht_in_queue_wenn_voll: OK\n");
+}
+
+int main(void)
+{
+    test_simulationsschritt_ankunft_geht_in_queue_wenn_voll();
+    return 0;
+}
