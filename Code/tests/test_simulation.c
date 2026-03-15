acdfd48d54ee4b326b23efc742afc7154f97a935
Roman Benz <kontakt@romanbenz.com>
2026-03-15 17:35:31 +0100

    Wrote comments that explain the Unit-Test

--- Code/tests/test_simulation.c
+++ Code/tests/test_simulation.c
@@ -5,29 +5,29 @@
 #include <stdlib.h>
 #include "../Include/Simulation.h"
 
-
+//Testet, ob ein Fahrzeug bei vollem Parkhaus korrekt in die Warteschlange aufgenommen wird
 void test_simulationsschritt_ankunft_geht_in_queue_wenn_voll(void)
 {
-    Parkhaus garage;
-    initialisierung_garage(&garage, 1);
+    Parkhaus garage;       
+    initialisierung_garage(&garage, 1); //Neues Parkhaus mit einem Parkplatz wird initialisiert
 
     Fahrzeug geparktes_auto = {1, 99, 0, 0}; 
-    einparken_fahrzeug(&garage, &geparktes_auto);
+    einparken_fahrzeug(&garage, &geparktes_auto);   //Fahrzeug wird auf dem einziegen Parkplatz geparkt
 
     Queue queue;
-    queue_init(&queue);
+    queue_init(&queue);    //Neue Warteschlange wird initialisiert
 
-    Simulationdaten daten = {0, 0, 0, 0, 0, 0, 0.0, 0.0, 0.0};
+    Simulationdaten daten = {0, 0, 0, 0, 0, 0, 0.0, 0.0, 0.0};  //Alle Statisitken werden auf 0 gesetzt
 
-    Simulationskonfiguration konfig = {1, 10, 100, 100, 42};
+    Simulationskonfiguration konfig = {1, 10, 100, 100, 42};    //Setzt unter anderem die Ankunftwahrscheinlichkeit von 100% fest
     srand(konfig.zufalls_seed);
 
     ausfuehren_simulationsschritt(1, &konfig, &garage, &queue, &daten);
 
-    assert(daten.gesamt_ankuenfte == 1);   
-    assert(queue.length == 1);             
-    assert(daten.gesamt_geparkt == 0);      
-    assert(garage.belegte_stellplaetze == 1); 
+    assert(daten.gesamt_ankuenfte == 1);   //Gesamtankünfte muss 1 sein
+    assert(queue.length == 1);             //Warteschlangelänge muss 1 sein
+    assert(daten.gesamt_geparkt == 0);     //Neue Eingeparkte Fahrzeuge muss 0 sein
+    assert(garage.belegte_stellplaetze == 1); //eingeparktes Fahrzeug von vor start muss noch da sein
 
     free(garage.p_stellplaetze);
     queue_destroy(&queue);
