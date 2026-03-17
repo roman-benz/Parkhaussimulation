56b2c61613d32ae32f0168d02c7a9e8ab52b8d73
Julien Armbruster <Julien.armbruster06@gmail.com>
2026-03-17 15:29:36 +0100

    string.h hinzugefügt damit ich die Ausgaben in den Textdateien besser vergleichen kann

--- Code/tests/test_simulation.c
+++ Code/tests/test_simulation.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../Include/Simulation.h"
 
 // Prüft erfolgreiche Initialisierung und definierten Startzustand aller Stellplätze
