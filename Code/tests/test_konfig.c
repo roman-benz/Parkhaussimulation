408b8e943c63789890f27d2b4cf29f98d6a06308
Marc Born <marc.born.mb@gmail.com>
2026-03-17 17:05:56 +0100

    Test für gültige Zahleneingabe hinzugefügt

--- Code/tests/test_konfig.c
+++ Code/tests/test_konfig.c
@@ -8,5 +8,14 @@
 
 void test_int_wert_einlesen_gueltige_eingabe(void)
 {
+    const char *pfad = "test_input_int_ok.txt";
+	int out_wert = -1;
 
+	assert(stdin_auf_datei_setzen(pfad, "42\n") == 1);
+
+	assert(int_wert_einlesen("Test: ", 1, 100, &out_wert) == 1);
+	assert(out_wert == 42);
+
+	remove(pfad);
+	printf("test_int_wert_einlesen_gueltige_eingabe: OK\n");
 }
